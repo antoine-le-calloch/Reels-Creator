196b750509a2042ae492f80c9935711e8793710f
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-07-22 00:13:51 -0700

    improve main. delete useless files.

--- src/main.py
+++ src/main.py
@@ -6,7 +6,10 @@ from youtube import (
 
 if __name__ == '__main__':
     most_replayed_infos = get_most_replayed_infos('x7bDk4Jvh6s')
-    draw_graph([[info['startMillis'], info['intensityScoreNormalized']] for info in most_replayed_infos['markers']])
+    points = []
+    for info in most_replayed_infos['markers']:
+        points.append([info['startMillis'], info['intensityScoreNormalized']])
+    draw_graph(points)
 
     most_replayed_points = get_most_replayed_points('x7bDk4Jvh6s')
     draw_graph(most_replayed_points)
